7749566ced16fbc686b2e7405f99779cc64b46f0
Ramy Silva <ramypdg@gmail.com>
2025-12-18 15:40:13 -0300

    test: add textbox spec test for demoqa automation

---
+++ cypress/e2e/elements/textbox.cy.js
@@ -0,0 +1,19 @@
+beforeEach(() => {
+    cy.visit('https://demoqa.com/text-box')
+  })
+
+describe('Text Box spec test',() =>{
+  Cypress.on('uncaught:exception', (err, runnable) => {return false;}) 
+  it('text box test', () => {   
+    cy.get('#userName').type('Ramy Silva')
+    cy.get('#userEmail').type('cypress@gmail.com')
+    cy.get('#currentAddress').type('test address 123')
+    cy.get('#permanentAddress').type('permanent address 456')
+    cy.get('#submit').click()
+    cy.contains('Ramy Silva').should('be.visible')
+    cy.contains('cypress@gmail.com').should('be.visible')
+    cy.contains('test address 123').should('be.visible')
+    cy.contains('permanent address 456').should('be.visible')
+  })
+
+}) 
